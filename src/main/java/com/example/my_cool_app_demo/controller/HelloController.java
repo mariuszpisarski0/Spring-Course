97eff6084b60c36aa975604a906fce9c81cd056d
Mariusz <mariuszpisarski0@gmail.com>
2024-12-16 18:29:21 +0100

    port and context path properities

--- src/main/java/com/example/my_cool_app_demo/controller/HelloController.java
+++ src/main/java/com/example/my_cool_app_demo/controller/HelloController.java
@@ -1,10 +1,21 @@
 package com.example.my_cool_app_demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teameName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return teameName + " " + coachName;
+    }
 
     @GetMapping("/")
     public String getHello() {
